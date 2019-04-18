252c116c5703df7544c6e706adcdb2203268afce
Ussama Azam <nuclearoreo@gmail.com>
2019-04-18 14:40:07 -0400

    Added new info component

--- app/components/Details/index.js
+++ app/components/Details/index.js
@@ -1,11 +1,13 @@
 import Header from './header';
 import Actions from './Actions';
 import Row from './row';
+import Info from './info';
 import styles from './styles';
 
 export {
     Header,
     Actions,
     Row,
+    Info,
     styles
 }
