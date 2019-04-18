43c1f95ce619d44e954038dd67125ee20d27b575
Ussama Azam <nuclearoreo@gmail.com>
2019-04-18 16:07:29 -0400

    Have both tab and stack navigation

--- app/config/router.js
+++ app/config/router.js
@@ -1,9 +1,9 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
-
-
+import Me from '../screens/Me';
+import NewContact from '../screens/NewContact';
 
 const ContactsStack = createStackNavigator({
     Contacts: {
@@ -20,4 +20,25 @@ const ContactsStack = createStackNavigator({
     }
 });
 
-export const AppContainer = createAppContainer(ContactsStack);
+const Tabs = createBottomTabNavigator({
+    Contacts: {
+        screen: ContactsStack,
+        navigationOptions: {
+            title: 'Contacts'
+        }
+    },
+    NewContact: {
+        screen: NewContact,
+        navigationOptions: {
+            title: 'New Contacts'
+        }
+    },
+    Me: {
+        screen: Me,
+        navigationOptions: {
+            title: 'Me'
+        }
+    }
+});
+
+export const AppContainer = createAppContainer(Tabs);
--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { AppContainer } from './config/router';
+import { AppContainer} from './config/router';
 
 
 const App = () => {
-    return <AppContainer />
+    return <AppContainer/>
 };
 
 export default App;
